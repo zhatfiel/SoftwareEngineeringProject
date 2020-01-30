cb7f4059901bdb08798193503c90648dbc0db3d1
zhatfield90 <zhatfiel@uncc.edu>
2020-01-30 04:54:30 +0000

    RubyBasics1 - Part 3 V2

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -18,24 +18,26 @@ def max_2_sum arr
   end
 end
 
-# Part III
 def sum_to_n? arr, n
-  # YOUR CODE HERE
-  if arr.length == 0
-    false
-  elsif arr.length == 1
-    false
-  elsif arr.length > 1
-    for x in arr
-      for y in arr[arr.index(x)+1..-1]
-        sum = x + y
-        equal = sum == n
-        break if equal
-      end
-      break if equal
-    end
-    equal
-  else
-    false
+  if (arr.length == 0)
+    return false
   end
+  if (arr.length == 1)
+    return false
+  end
+  sorted_arr = arr.sort
+  head =0
+  tail = sorted_arr.length-1
+  
+  while head < tail 
+    current_sum = sorted_arr[head] + sorted_arr[tail]
+    if current_sum == n
+      return true
+    elsif current_sum < n
+      head = head + 1
+    else 
+      tail = tail - 1
+    end
+  end
+  return false
 end
