1e05ccb6d7681673cb8f759386242a357daad4aa
zhatfield90 <zhatfiel@uncc.edu>
2020-02-04 05:58:08 +0000

    RubyHashes - Part 1

--- RubyHashes/lib/hashes.rb
+++ RubyHashes/lib/hashes.rb
@@ -2,11 +2,22 @@
 # Part I
 def array_2_hash emails, contacts
     # YOUR CODE HERE
+    if !emails.empty?
+        i=0
+        contacts.each do |key, value|
+            contacts.store(key, emails[i])
+            i+=1
+        end
+    else
+        return contacts
+    end
+    #puts contacts
 end
 
 # Part II
 def array2d_2_hash contact_info, contacts
     # YOUR CODE HERE
+    
 end
 
 # Part III
