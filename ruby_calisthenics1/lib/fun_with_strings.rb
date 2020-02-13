d12e230fbb35796ada9e194a88d78c84f94ee20d
zhatfield90 <zhatfiel@uncc.edu>
2020-02-13 04:18:49 +0000

    Lab 9: Fun With Words - Palindrome

--- ruby_calisthenics1/lib/fun_with_strings.rb
+++ ruby_calisthenics1/lib/fun_with_strings.rb
@@ -1,12 +1,19 @@
 module FunWithStrings
   def palindrome?
     # your code here
+    temp1 = self.downcase
+    temp2 = self.downcase
+    temp2.reverse!
+    return true if (temp1.scan(/\w/) == temp2.scan(/\w/))
+    false
   end
   def count_words
     # your code here
+    
   end
   def anagram_groups
     # your code here
+    
   end
 end
 
