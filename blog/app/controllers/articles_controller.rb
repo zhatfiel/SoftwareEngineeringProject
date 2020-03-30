9784e387b3674eb9c118e45479d81f4449a4439b
zhatfield90 <zhatfiel@uncc.edu>
2020-03-30 22:46:31 +0000

    Add Article List link to show

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -31,7 +31,7 @@ class ArticlesController < ApplicationController
     def update
         @article = Article.find(params[:id]) 
         
-        if Article.update(article_params)
+        if @article.update(article_params)
             redirect_to @article
         else
             render 'edit'
