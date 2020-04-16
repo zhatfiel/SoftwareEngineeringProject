ebd152efbef648e9da89914347125bdc11b94e20
zhatfield90 <zhatfiel@uncc.edu>
2020-04-16 17:51:35 +0000

    adding user functionality

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -17,6 +17,7 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         #byebug
+        @article.user = current_user
         
         if @article.save
             #byebug
